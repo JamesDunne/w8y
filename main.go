63c78c324a800066248300527f0fa92015c3b1a8
jsd1982 <james.jdunne@gmail.com>
2022-03-20 14:09:29 -0500

    Configurable REDIS_URL env var; slightly better error handling

--- main.go
+++ main.go
@@ -4,20 +4,38 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"log"
+	"os"
 	"time"
 )
 
 func main() {
-	rds := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
+	var err error
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.LUTC)
+
+	// parse REDIS_URL for connection info:
+	var options *redis.Options
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		redisUrl = "redis://localhost:6379"
+	}
+
+	options, err = redis.ParseURL(redisUrl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var rds *redis.Client
+	rds = redis.NewClient(options)
 	defer func() {
-		rds.Close()
+		err = rds.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}()
 
 	ctx := context.Background()
 
-	var err error
 	var keyPrefix = "tenants"
 	var keyList = keyPrefix + ":list"
 	var keyProcessing string
@@ -28,7 +46,7 @@ func main() {
 	// iteration but this is okay since we can always restart and pick up the new list size.
 	var listLen int64
 	if listLen, err = rds.LLen(ctx, keyList).Result(); err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	var processingKeyExists int64
@@ -37,13 +55,13 @@ func main() {
 	for i := int64(0); i < listLen; i++ {
 		// pop from left side of list and atomically append to right side of list:
 		if tenantId, err = rds.LMove(ctx, keyList, keyList, "left", "right").Result(); err != nil {
-			panic(err)
+			log.Fatalln(err)
 		}
 
 		// check processing marker:
 		keyProcessing = fmt.Sprintf("%s:%s", keyPrefix, tenantId)
 		if processingKeyExists, err = rds.Exists(ctx, keyProcessing).Result(); err != nil {
-			panic(err)
+			log.Fatalln(err)
 		}
 		if processingKeyExists == 0 {
 			// no processor key exists for this item so let's grab it:
@@ -59,7 +77,9 @@ func main() {
 	}
 
 	// mark this record as being processed; intended to be a keep-alive every N seconds:
-	rds.SetEX(ctx, keyProcessing, 1, time.Second*5)
+	if _, err = rds.SetEX(ctx, keyProcessing, 1, time.Second*5).Result(); err != nil {
+		log.Fatalln(err)
+	}
 
 	fmt.Println(tenantId)
 }
